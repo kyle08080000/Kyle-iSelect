00a5d6b65aa82acb8235388a389e4516b517a0fc
kyle08080000 <s106441109@gmail.com>
2024-02-19 22:54:22 +0800

    Updates

--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import  messageReducer  from "./slice/messageSlice";
-
-export const store = configureStore({
-  reducer: {
-    message: messageReducer,
-  }
-});
-
